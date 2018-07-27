c6db286bf1b7fd74003f1803e539e534109e34a4
DESKTOP-2AG7N6I\josea <josealbertodeleon8@outlook.com>
2018-07-27 00:30:21 -0400

    Iinital Commit

---
+++ src/Carousel.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class='carousel-view'>
+    <h2>Ejemplo Slider</h2>
+      <div>
+        <img :src="currentImage">
+      </div>
+      <div>
+        <button @click="previous">prev</button>
+        <button @click="next">next</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'carousel',
+
+  data() {
+    return {
+      currentNumber: 0,
+      slides: ['https://www.import.io/wp-content/uploads/2017/10/React-logo.png',
+              'https://upload.wikimedia.org/wikipedia/commons/5/53/Vue.js_Logo.svg',
+              'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg']
+    }
+  },
+
+  methods: {
+    next() {
+      this.currentNumber++;
+      console.log(this.currentNumber);
+
+    },
+    previous() {
+      this.currentNumber--;
+    }
+  },
+
+  computed: {
+    currentImage() {
+      if(Math.abs(this.currentNumber) > (this.slides.length - 1))
+        this.currentNumber = 0;
+      return this.slides[Math.abs(this.currentNumber)];
+    }
+  }
+}
+</script>
